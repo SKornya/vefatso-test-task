60513e5b7e0b67a7d198b35a04370c9ea617293b
SKornya <kornyakovS@gmail.com>
2024-03-03 21:30:25 +0300

    remove

--- app/news/[id]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spin } from 'antd';
-import { FunctionComponent } from 'react';
-
-const Spinner: FunctionComponent = () => {
-  console.log('spin');
-  return <Spin style={{ margin: '0 auto' }} />;
-};
-
-export default Spinner;
